119c5dfd0b4e2dd287b310aca25df47d3b543c8a
SHIN SUJUNG <shinssjj79@gmail.com>
2024-12-15 11:23:17 +0900

    Initial commit

---
+++ src/main/java/com/example/maze_game2/MazeGame2Application.java
@@ -0,0 +1,13 @@
+package com.example.maze_game2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazeGame2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazeGame2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maze_game2/MazeGame2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maze_game2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazeGame2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
